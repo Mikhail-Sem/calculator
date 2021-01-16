d7eaf9229dc068b132c52d24379b635b1e61d8fe
Mikhail <mihalich013@mail.ru>
2021-01-16 14:26:41 +0300

    + добавил ввод арифметического выражения с консоли

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,38 +6,22 @@ public class Calculator {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String [] args) {
-        int num1 = getInt();
-        int num2 = getInt();
-        char operation = getOperation();
-        int result = calc(num1, num2, operation);
-        System.out.println("результат операции:" + result);
+        String $inputString = getString();
+
+        System.out.println($inputString);
+        /*int result = calc(num1, num2, operation);
+        System.out.println("результат операции:" + result);*/
     }
 
-    public static int getInt() {
-        System.out.println("введите 2 целых числа от 1 до 10 и операцию между ними:");
-        int num;
-        if (scanner.hasNextInt()) {
-            num = scanner.nextInt();
-        } else {
-            System.out.println("ошибка, попробуйте еще раз");
-            scanner.next();
-            num = getInt();
-        }
-        return num;
+    public static String getString() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите арифметическое выражение:");
+        String $input = in.nextLine();
+
+        return $input;
     }
-    public static char getOperation () {
-        System.out.println ("Введите операцию");
-        char operation;
-        if (scanner.hasNext()) {
-            operation = scanner.next().charAt(0);
-        } else {
-            System.out.println("ошибка, попробуйте еще раз");
-            scanner.next();
-            operation = getOperation();
-        }
-        return operation;
-    }
-    public static int calc(int num1, int num2, char operation) {
+
+    /*public static int calc(int num1, int num2, char operation) {
         int result;
         switch (operation) {
             case '+':
@@ -57,7 +41,5 @@ public class Calculator {
                 result = calc(num1, num2, getOperation());
         }
         return result;
-    }
+    }*/
 }
-
-1
